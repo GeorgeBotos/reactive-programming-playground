0100035e38a23dd1c34a6ed7957c83274e209453
george.botos.no1@gmail.com <george.botos.no1@gmail.com>
2024-05-18 01:06:14 +0100

    Assignment added

---
+++ src/main/java/org/botos/assignment/Assignment.java
@@ -0,0 +1,21 @@
+package org.botos.assignment;
+
+import static org.botos.common.Util.subscriber;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+
+		var fileService = new FileServiceImpl();
+		var fileName = "file.txt";
+
+		fileService.write(fileName, "This is the content of the file.")
+		           .subscribe(subscriber());
+
+		fileService.read(fileName)
+		           .subscribe(subscriber());
+
+		fileService.delete(fileName)
+		           .subscribe(subscriber());
+	}
+}
--- src/main/java/org/botos/sec02/NonBlockingIO.java
+++ src/main/java/org/botos/sec02/NonBlockingIO.java
@@ -1,4 +1,24 @@
 package org.botos.sec02;
 
+import lombok.extern.slf4j.Slf4j;
+import org.botos.sec02.client.ExternalServiceClient;
+
+import static org.botos.common.Util.sleep;
+import static org.botos.common.Util.subscriber;
+
+@Slf4j
 public class NonBlockingIO {
+
+	public static void main(String[] args) {
+
+		var client = new ExternalServiceClient();
+
+		log.info("starting");
+
+		for (int i = 1; i <= 100; i++) {
+			client.getProductName(i)
+			      .subscribe(subscriber());
+		}
+		sleep(2);
+	}
 }
